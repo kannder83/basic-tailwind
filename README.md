c96c17d9498b16266f4ae518dd25c7c8c42df89d
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-02-25 15:28:46 -0500

    new secctions add to home page

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-- Commands
+### Practice with Tailwind
 
-npx tailwindcss -i ./src/css/tailwind.css -o ./public/css/tailwind.css --watch
+It's a repository of my practice with Tailwild, just enjoy!
+
+###Use this commands:
+
+##For work and see changes online, you can use this command:
+
+`npm run tw:dev`
+
+But if you whan build the project, just run this:
+
+`npm run tw:build`
+
+
+###See my porfolio at:
+
+[kannder83](https://kannder83.com "kannder83")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'hero': "url('/public/img/guillaume-briard-lSXpV8bDeMA-unsplash.jpg')",
+        'mountain-01': "url('/public/img/benjamin-voros-phIFdC6lA4E-unsplash.jpg')",
+        'mountain-02': "url('/public/img/jeremy-bishop-dvACrXUExLs-unsplash.jpg')"
+      }
+    },
   },
   plugins: [],
 };
